f1fcca24cbf45e59bef5c8cf047f6ed8d2da714e
Uri Arev <uri.arev@gmail.com>
2021-02-17 10:51:10 +0200

    [patch] Making the file more like Site-API-Go

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
-import sys
 
-if len(sys.argv) == 3:
-    try:
-        response = requests.get("http://127.0.0.1:80/api", headers = { "username": sys.argv[1], "password": sys.argv[2]} )
-    except requests.exceptions.ConnectionError:
-        response = False
-        print("[\033[91m!\033[0m] [API] Connection error")
-    if response:
-        print("[\033[92m*\033[0m] Response: {}".format(response.text).replace("\n", ""))
+user = input("USERNAME: ")
+passwd = input("PASSWORD: ")
+
+while True:
+    dbg = input(">")
+    if dbg == "debug":
+        try:
+            response = requests.get("http://127.0.0.1:80/api", headers = { "username": user, "password": passwd, "user-agent": "Official Python Wantyapps Site API" } )
+        except requests.exceptions.ConnectionError:
+            response = False
+            print("[\033[91m!\033[0m] [API] Connection error")
+        if response:
+            print("[\033[92m*\033[0m] Response: {}".format(response.text).replace("\n", ""))
+
